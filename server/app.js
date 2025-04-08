05ee917d500bbcc52619def168aa371a895cb8fb
SaurabhWagh07 <154869876+SaurabhWagh07@users.noreply.github.com>
2025-04-08 18:55:00 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ dotenv.config({path: "./config/config.env"});
 
 app.use(cors({
     // origin: [process.env.FRONTEND_URL],
-    origin: [`process.env.https://curious-truffle-8eed98.netlify.app/`],
+    origin: [`process.env.https://curious-truffle-8eed98.netlify.app/`] || 10000,
     methods: ["POST"],
     credentials:true 
 }));
